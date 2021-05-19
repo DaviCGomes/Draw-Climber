5480facdeff7ac73219a966e7956728baeaa7a1a
unknown <davi62gomes@gmail.com>
2021-05-18 22:41:29 -0300

    Comentado

--- Assets/Script/DrawClimber.cs
+++ Assets/Script/DrawClimber.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 public class DrawClimber : MonoBehaviour {
-    
+    //variáveis não encapsuladar para alteração no editor
     public Player player;
     public float finishLine;
     public Shader shader;
@@ -12,20 +10,34 @@ public class DrawClimber : MonoBehaviour {
     public GameObject leftL, rightL;
     public GameObject drawObj;
 
-    // Start is called before the first frame update
+    //Inicializa as variáveis do player
     void Awake () {
+        //Inicializar o Rigidbody e a velocidade do jogador
+        player.Speed = 5f;
         player.Rigidbody = player.GetComponent<Rigidbody>();
-        player.Animator = player.GetComponent<Animator>();
         player.Rigidbody.useGravity = false;
+
+        //Inicializa o shader, cor do traço e os objetos3d
         player.Shader = shader;
         player.VertexPrecision = vertexPrecision;
-        player.LeftL = leftL;
-        player.RightL = rightL;
         player.DrawObject = drawObj;
+        player.Color = new Color(0, 0, 0, 1);
+
+        //Inicializa o eixo de rotação e velocidade da perna esquerda
+        player.LeftL = leftL;
+        player.LeftL.GetComponent<Rotation>().Angle = new Vector3(0, 0, -1);
+        player.LeftL.GetComponent<Rotation>().Speed = 500.0f;
+        player.LeftL.GetComponent<Rotation>().Rotate = false;
+
+        //Inicializa o eixo de rotação e velocidade da perna direita
+        player.RightL = rightL;
+        player.RightL.GetComponent<Rotation>().Angle = new Vector3(0, 0, -1);
+        player.RightL.GetComponent<Rotation>().Speed = 500.0f;
+        player.RightL.GetComponent<Rotation>().Rotate = false;
     }
 
-    // Update is called once per frame
     void Update() {
+        //Define a direção do jogador e habilita a gravidade quando o jogador estiver correndo
         if(player.Run) {
             player.Rigidbody.useGravity = true;
             if(player.IsGrounded)
@@ -34,6 +46,8 @@ public class DrawClimber : MonoBehaviour {
             player.Rigidbody.useGravity = false;
             player.Rigidbody.velocity = transform.TransformVector(0, 0, 0);
         }
+
+        //O jogador para ao chegar na linha de chegada
         if(player.transform.position.x == finishLine) {
             player.Rigidbody.useGravity = false;
             player.Rigidbody.velocity = transform.TransformVector(0, 0, 0);
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour {
-    public bool FirstDraw {
-        get;
-        set;
-    }
+    //Variáveis encapsladar
     public float Speed {
         get;
         set;
@@ -21,10 +18,6 @@ public class Player : MonoBehaviour {
         set;
     }
     public Color Color {
-        get;
-        set;
-    }
-    public Animator Animator {
         get;
         set;
     }
@@ -57,21 +50,14 @@ public class Player : MonoBehaviour {
         set;
     }
 
+    //Inicializar as variáveis
     private void Start() {
-        Speed = 5f;
-        FirstDraw = false;
-        Color = new Color(0, 0, 0, 1);
+        //Inicializa a distancia para a verificação se o jogador está no chão
         Distance = 1f;
-        LeftL.GetComponent<Rotation>().Angle = new Vector3(0, 0, -1);
-        LeftL.GetComponent<Rotation>().Speed = 500.0f;
-        LeftL.GetComponent<Rotation>().Rotate = false;
-        RightL.GetComponent<Rotation>().Angle = new Vector3(0, 0, -1);
-        RightL.GetComponent<Rotation>().Speed = 500.0f;
-        RightL.GetComponent<Rotation>().Rotate = false;
     }
 
-    // Update is called once per frame
     void Update() {
+        //Ativa e desativa a rotação das pernas quando o jogador está ou não se movendo 
         if(Run) {
             LeftL.GetComponent<Rotation>().Rotate = true;
             RightL.GetComponent<Rotation>().Rotate = true;
@@ -79,25 +65,36 @@ public class Player : MonoBehaviour {
             LeftL.GetComponent<Rotation>().Rotate = false;
             RightL.GetComponent<Rotation>().Rotate = false;
         }
+
+        //Permite ao jogador desenhar na tela e cirar as pernas
         if(Input.GetMouseButtonDown(0)) {
             if(EventSystem.current.IsPointerOverGameObject())
                 StartCoroutine(draw());
         }
+        
+        //Verifica se o jogador está no chão;
         CheckGround();
+
+        //O jogador volta ao inicio caso ele caia
         if(transform.position.y < 0f) {
             transform.position = new Vector3(0, 3f, 0);
         }
     }
 
+    //Variáveis para o desenho
     Vector3 newVertex;
     Vector3 lastVertex;
     IEnumerator draw() {
+        //Quando o jogador começa a desenhar o personagem para de correr
         Run = false;
+
+        //Reseta a distancia de detecção ao chão
         Distance = 0f;
 
+        //Destroi as pernas antigas
         DrawnDestroy();
 
-        //Setting up the line render
+        //Inicializa as variáveis de desenho
         LineRenderer r = new GameObject().AddComponent<LineRenderer>();
         r.transform.SetParent(transform);
         r.startWidth = 0.3f;
@@ -108,9 +105,12 @@ public class Player : MonoBehaviour {
 
         List<Vector3> positions = new List<Vector3>();
 
-        while(Input.GetMouseButton(0)){ // Adding Mouse Points To Line Render
+        //Adiciona os pontos ao LineRender
+        while(Input.GetMouseButton(0)){
             newVertex = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 5;
-            if(Vector3.Distance(lastVertex, newVertex) >= VertexPrecision){ //Checking distance between vertx
+
+            //Verifica a distancia entre os verteces
+            if(Vector3.Distance(lastVertex, newVertex) >= VertexPrecision){
                 positions.Add(newVertex);
                 r.positionCount = positions.Count;
                 r.SetPositions(positions.ToArray());
@@ -119,20 +119,31 @@ public class Player : MonoBehaviour {
             yield return new WaitForEndOfFrame();
         }
 
+        //Cria objetos para a formação das pernas
         r.useWorldSpace = false;
         Vector3 aux = positions[0];
         for(int i = 0; i < positions.Count; i++) {
+            
+            //Cria a perna esquerda no 3º e 4º quadrande
             Instantiate(DrawObject, new Vector3(((positions[i].x - aux.x)/2f) + transform.position.x , ((positions[i].y - aux.y) / 2f) + transform.position.y, LeftL.transform.position.z), new Quaternion(), LeftL.transform);
+
+            //Cria a perna direita no 1º e 2º quadrande
             Instantiate(DrawObject, new Vector3(((aux.x - positions[i].x)/2f) + transform.position.x , (((aux.y - positions[i].y) / 2f) + transform.position.y), RightL.transform.position.z), new Quaternion(), RightL.transform);
+            
+            //Procura o ponto mais baixo para criar a distancia do chão
             if(positions[i].y < 0)
                 if(positions[i].y < Distance)
                     Distance = positions[i].y;
         }
 
+        //Ajeita a distancia do chão
         Distance = ((Distance - aux.y) / 2) + transform.position.y;
+
+        //O personagem começa a correr
         Run = true;
     }
 
+    //Verifica se o personagem está em contato com o chão
     private void CheckGround() {
         Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
         Vector3 direction = transform.TransformDirection(1f,-1f,0);
@@ -145,13 +156,18 @@ public class Player : MonoBehaviour {
         }
     }
 
+    //Destroi os objetos relacionados com o desenho
     private void DrawnDestroy() {
+
+        //Destroi os objetos 3d nas pernas
         GameObject[] game = GameObject.FindGameObjectsWithTag("DrawnObject");
         if(game != null) {
             foreach(GameObject dO in game) {
                 Destroy(dO.gameObject);
             }
         }
+
+        //Destroi a lina desenhada
         game = GameObject.FindGameObjectsWithTag("Line");
         if(game != null) {
             foreach(GameObject dO in game) {
